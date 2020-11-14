fba70fa532934fd306eb2da2e2826e3e101d24d5
quietgit <72638261+quietgit@users.noreply.github.com>
2020-11-13 16:36:17 -0800

    Update Summary Values.R

--- Summary Values.R
+++ Summary Values.R
@@ -20,34 +20,34 @@ hc_summary <- get_summary_info(hc)
 ##Calculated Values
 
 # maximum beds (state)
-max_sys_beds <- hc
-  filter(sys_beds == max(sys_beds)) 
+max_sys_beds <- hc %>%
+  filter(sys_beds == max(sys_beds)) %>%
   pull(health_sys_state)
 
 # minimum beds (state)
-min_sys_beds <- hc
-  filter(sys_beds == min(sys_beds)) 
+min_sys_beds <- hc %>%
+  filter(sys_beds == min(sys_beds)) %>%
   pull(health_sys_state)
 
 # state with most medics
-health_sys_state_max_mds <- hc
-  group_by(health_sys_state) 
-  summarize(total = sum(total_mds))
-  filter(total == max(total))
+health_sys_state_max_mds <- hc %>%
+  group_by(health_sys_state)  %>%
+  summarize(total = sum(total_mds)) %>%
+  filter(total == max(total)) %>%
   pull(health_sys_state)
 
 # most medics
-max_total_mds <- hc
-  filter(total_mds == max(total_mds))
+max_total_mds <- hc %>%
+  filter(total_mds == max(total_mds)) %>%
   pull(health_sys_city)
 
 # least medics
-min_total_mds <- hc
-  filter(total_mds == min(total_mds))
+min_total_mds <- hc %>%
+  filter(total_mds == min(total_mds)) %>%
   pull(health_sys_city)
 
 # sum of medics by state
-sum_total_mds <- hc
-  group_by(health_sys_state)
-  filter(sum_total_mds == sum(total_mds)) 
+sum_total_mds <- hc %>%
+  group_by(health_sys_state) %>%
+  filter(sum_total_mds == sum(total_mds))  %>%
   pull(sum_total_mds)
